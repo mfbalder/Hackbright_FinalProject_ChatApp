1f170ece8433807d43cbd6b350c16c1e08b9dc50
Micki <mfbalder@gmail.com>
2014-11-06 10:42:49 -0800

    Initial commit: Users can login, save their identity to the session, join a room, and talk to anyone in that room directly

---
+++ webapp.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, session, request, redirect
+from flask.ext.socketio import SocketIO, emit, join_room, leave_room, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+app.secret_key = "ABC"
+
+room = None
+user = None
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/login')
+def login():
+	return render_template("login.html")
+
+@app.route("/set_session")
+def set_session():
+	user = request.args.get("user")
+	session["user"] = user
+	print session
+	return redirect("/")
+
+@socketio.on('my event', namespace='/chat')
+def test_message(message):
+	global user
+	print message['data']
+	emit('my response', {'data': message['data'], 'user': session['user']}, room=room)
+
+@socketio.on('join', namespace='/chat')
+def on_join(data):
+	global room, user
+	print data
+	user = data['username']
+	room = data['room']
+	join_room(room)
+	print "Room joined %s" % room
+	emit('my response', {'data': "Success!", 'user': session['user']}, room=room)
+
+@socketio.on('connect', namespace='/chat')
+def test_connect():
+	print('Connected')
+	emit('connected', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/chat')
+def test_disconnect():
+    print('%s Client disconnected') % session['user']
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+
+# 
