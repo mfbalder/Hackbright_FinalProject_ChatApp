384c0a8824840627d32049fe7d83b15aa8b4d620
Micki <mfbalder@gmail.com>
2014-11-11 16:01:37 -0800

    Fixed styling of chat boxes. Fixed log scrolling

--- webapp.py
+++ webapp.py
@@ -142,7 +142,7 @@ def open_chat(data):
 	receiving_user = data['receiving']
 	
 	# open a chat window in both users' windows
-	emit('open chat box', {'template': render_template("chat_box.html", room=room, receiving_user=receiving_user)}, room=submitting_user)
+	emit('open chat box', {'template': render_template("chat_box.html", room=room, receiving_user=receiving_user), 'receiving_user': receiving_user}, room=submitting_user)
 	emit('open chat box', {'template': render_template("chat_box.html", room=room, receiving_user=submitting_user)}, room=receiving_user)
 
 
